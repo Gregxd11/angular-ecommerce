430ec137bee5f34a594187c3c284e61016c90763
Greg Frediani <fredianig11@gmail.com>
2020-10-04 00:03:08 +0200

    added api call to fake ecommerce api/styled product page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: [ './products.component.scss' ]
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  products = [];
+  constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
+    this.productsService.getRandomProducts().subscribe(res => {
+      this.products = res;
+      console.log(this.products);
+    });
   }
-
 }
